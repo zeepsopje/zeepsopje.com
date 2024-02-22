47c9ca6603dbb063fb15469455e66c761d89a0cf
zeepsopje <stefanhuynh98@gmail.com>
2024-02-22 14:16:23 +0100

    remove svelticon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import RasterImage from '$lib/RasterImage.svelte';
 	import Section from '$lib/Section.svelte';
 	import ProjectList from '$lib/ProjectList.svelte';
-	import * as Icon from 'svelticon';
 </script>
 
 <div class="image">
@@ -14,7 +13,7 @@
 		<h2>Stefan Huynh</h2>
 		<h3>Just a curious programmer.</h3>
 		<a class="icon" href="https://github.com/zeepsopje" target="_blank">
-			<Icon.Github color="#fff" />
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/></svg>
 		</a>
 	</Section>
 	<Section name="about">
